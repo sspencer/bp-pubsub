e5f00868b82db278b6f1303a68cc0bd628f2db0f
Gun.io Whitespace Robot <contact@gun.io>
2011-10-28 11:05:35 -0400

    Remove whitespace [Gun.io WhitespaceBot]

--- service/pubsub.rb
+++ service/pubsub.rb
@@ -6,12 +6,12 @@ require 'uri'
 # Simplified Observable module - notify observers of changed data.
 #
 class PSObservable
-  
+
   # observer has to respond to "update"
   def add_observer(observer)
     @observer_peers = [] unless defined? @observer_peers
     unless observer.respond_to? :update
-      raise NoMethodError, "observer needs to respond to 'update'" 
+      raise NoMethodError, "observer needs to respond to 'update'"
     end
     @observer_peers.push observer
   end
@@ -39,7 +39,7 @@ end
 class PubSub
 
   # Creation triggered through BrowserPlus
-  def initialize(context)  
+  def initialize(context)
     uri = URI.parse(context['uri'])
 
     if uri.scheme == "file"
@@ -53,14 +53,14 @@ class PubSub
     # origin is "http://www.example.com" or "file://"
     @origin = "#{uri.scheme}://#{domain}"
 
-    # one service instance can have multiple subscribers 
+    # one service instance can have multiple subscribers
     @subscribers = []
-    
+
     # unique object to signal data types that shouldn't be transfered through postMessage
     @@DontCopy = Object.new
-  end  
-  
-  
+  end
+
+
   # Log through BrowserPlus and text file if you need to
   def dbg(o)
     msg = o.class.name == "String" ? o : o.pretty_inspect
@@ -111,7 +111,7 @@ class PubSub
         # bonus (not HTML5) - allow client to filter here and not in JavaScript
         if (s["filter"] == "*" || s["filter"] == msgOrigin)
           s["callback"].invoke({"data"=> data, "origin" => msgOrigin})
-        end #if 
+        end #if
       end #each_key
     end #if
   end #def
@@ -123,7 +123,7 @@ class PubSub
 
     # become an observer
     PSObservable.instance.add_observer(self) if (@subscribers.count == 1)
-    
+
     # NEVER CALL bp.complete or callback will become invalid
   end
 
@@ -142,34 +142,34 @@ class PubSub
   end
 
 end #class
-  
-  
-rubyCoreletDefinition = {  
-  'class' => "PubSub",  
-  'name' => "PublishSubscribe",  
-  'major_version' => 1,  
-  'minor_version' => 0,  
-  'micro_version' => 0,  
-  'documentation' => 'A cross document message service that allows JavaScript to send and receive messages between ' + 
+
+
+rubyCoreletDefinition = {
+  'class' => "PubSub",
+  'name' => "PublishSubscribe",
+  'major_version' => 1,
+  'minor_version' => 0,
+  'micro_version' => 0,
+  'documentation' => 'A cross document message service that allows JavaScript to send and receive messages between ' +
     'web pages within one or more browsers (cross document + cross process).',
-  'functions' =>  
-  [  
-    {  
-      'name' => 'addListener',  
+  'functions' =>
+  [
+    {
+      'name' => 'addListener',
       'documentation' => "Subscribe to the pubsub mechanism.",
-      'arguments' => [  
-        {  
+      'arguments' => [
+        {
           'name' => 'receiver',
-          'type' => 'callback',  
-          'documentation' => 'JavaScript function that is notified of a message.  The value passed to the callback ' + 
+          'type' => 'callback',
+          'documentation' => 'JavaScript function that is notified of a message.  The value passed to the callback ' +
             'contains {data:(Any), origin:(String)}',
-          'required' => true  
+          'required' => true
         },
         {
           'name' => 'origin',
           'type' => 'string',
-          'documentation' => 'Optional string that specifies the domain ("http://example.com") to accept messages ' + 
-            'from.  Defaults to all ("*").  This is not part of the HTML5 spec but allows automatic filtering of ' + 
+          'documentation' => 'Optional string that specifies the domain ("http://example.com") to accept messages ' +
+            'from.  Defaults to all ("*").  This is not part of the HTML5 spec but allows automatic filtering of ' +
             'events so JavaScript listener does not have to manually check event.origin.',
           'required' => false
         }
@@ -177,24 +177,24 @@ rubyCoreletDefinition = {
     },
     {
       'name' => 'postMessage',
-      'documentation' => 'Post a message.  The message posted is associated with the domain of the sender.  ' + 
+      'documentation' => 'Post a message.  The message posted is associated with the domain of the sender.  ' +
         'Receivers may elect to filter messages based on the domain.',
       'arguments' => [
         {
           'name' => 'data',
           'type' => 'any',
-          'documentation' => 'The data object (Object, Array, String, Boolean, Integer, Float, Boolean, Null) that ' + 
+          'documentation' => 'The data object (Object, Array, String, Boolean, Integer, Float, Boolean, Null) that ' +
             'is posted to all interested subscribers.  All other data types are stripped out of the passed object.',
           'required' => true
         },
         {
           'name' => 'targetOrigin',
           'type' => 'string',
-          'documentation' => 'The origin specifies where to send the message to.  Options are either an URI like ' + 
+          'documentation' => 'The origin specifies where to send the message to.  Options are either an URI like ' +
             '"http://example.org" or "*" to pass it to all listeners.',
           'required' => true
         }
       ]
     }
-  ]   
+  ]
 }
